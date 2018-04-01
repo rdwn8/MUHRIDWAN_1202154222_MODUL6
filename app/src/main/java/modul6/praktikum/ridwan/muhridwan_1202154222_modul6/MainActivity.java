f5a0775ba7af7e1d73bbe5ea0f6440a354f8f116
rdwn8 <adamthesky@gmail.com>
2018-04-01 21:17:08 +0700

    commit 1 ridwan

---
+++ app/src/main/java/modul6/praktikum/ridwan/muhridwan_1202154222_modul6/MainActivity.java
@@ -0,0 +1,13 @@
+package modul6.praktikum.ridwan.fanharilardian_1202154222_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
